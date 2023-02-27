61f84f8706d72d6bf47a125c56a174e6bc46b85b
Victor <ivanvikvik@gmail.com>
2023-02-27 18:17:27 +0300

    Update util.cpp
    change rand_init function

--- Lesson45Project/util.cpp
+++ Lesson45Project/util.cpp
@@ -5,10 +5,6 @@
 using namespace std;
 
 void rand_init(int* array, int size, int bound) {
-	for (int i = 0; i < size; i++)
-	{
-		array[i] = rand() % bound;
-	}
 }
 
 string convert(int* array, int size) {
